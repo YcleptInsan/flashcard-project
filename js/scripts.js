182db7e1d1f62f068b05126c543ff4b56b432c95
nw <nickmwise@gmail.com>
2017-01-10 16:52:38 -0800

    added clickable/toggled paragraphs to display hidden definitions

---
+++ js/scripts.js
@@ -0,0 +1,113 @@
+$(function (){
+  $("#javascript").prev().click(function() {
+    $("#javascript").toggle();
+  });
+});
+
+$(function (){
+  $("#operator").prev().click(function() {
+    $("#operator").toggle();
+  });
+});
+
+$(function (){
+  $("#variables").prev().click(function() {
+    $("#variables").toggle();
+  });
+});
+
+$(function (){
+  $("#variable-naming-conventions").prev().click(function() {
+    $("#variable-naming-conventions").toggle();
+  });
+});
+
+$(function (){
+  $("#functions").prev().click(function() {
+    $("#functions").toggle();
+  });
+});
+
+$(function (){
+  $("#methods").prev().click(function() {
+    $("#methods").toggle();
+  });
+});
+
+$(function (){
+  $("#arguments").prev().click(function() {
+    $("#arguments").toggle();
+  });
+});
+
+$(function (){
+  $("#parameters").prev().click(function() {
+    $("#parameters").toggle();
+  });
+});
+
+$(function (){
+  $("#return").prev().click(function() {
+    $("#return").toggle();
+  });
+});
+
+$(function (){
+  $("#chaining-methods").prev().click(function() {
+    $("#chaining-methods").toggle();
+  });
+});
+
+$(function (){
+  $("#strings").prev().click(function() {
+    $("#strings").toggle();
+  });
+});
+
+$(function (){
+  $("#booleans").prev().click(function() {
+    $("#booleans").toggle();
+  });
+});
+
+$(function (){
+  $("#undefined").prev().click(function() {
+    $("#undefined").toggle();
+  });
+});
+
+$(function (){
+  $("#NaN").prev().click(function() {
+    $("#NaN").toggle();
+  });
+});
+
+$(function (){
+  $("#escape").prev().click(function() {
+    $("#escape").toggle();
+  });
+});
+
+$(function (){
+  $("#alert").prev().click(function() {
+    $("#alert").toggle();
+  });
+});
+
+$(function (){
+  $("#comments").prev().click(function() {
+    $("#comments").toggle();
+  });
+});
+
+$(function (){
+  $("#jQuery").prev().click(function() {
+    $("#jQuery").toggle();
+  });
+});
+
+$(function (){
+  $("#attributes").prev().click(function() {
+    $("#attributes").toggle();
+  });
+});
